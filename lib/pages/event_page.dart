3ad550494ea4b719b632baece298885b71e13b3b
Ayoub Adouche <ayad.devweb75@gmail.com>
2022-11-02 13:30:11 +0100

    Add builder For card

--- lib/pages/event_page.dart
+++ lib/pages/event_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class EventPage extends StatelessWidget {
+class EventPage extends StatefulWidget {
   const EventPage({Key? key}) : super(key: key);
 
+  @override
+  State<EventPage> createState() => _EventPageState();
+}
+
+class _EventPageState extends State<EventPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,26 +19,10 @@ class EventPage extends StatelessWidget {
           children: [
             Card(
               child: ListTile(
-                leading: Image.asset("assets/img/damien.jpg"),
-                title: Text('Damien Cavaillès(17h30 à 18h'),
-                subtitle: Text('git blame --no-offense ?'),
-                trailing: Icon(Icons.more_vert),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Image.asset("assets/img/defendintelligence.jpg"),
-                title: Text('Defend intelligence (18h à 18h30)'),
-                subtitle: Text('A la decouverte des IA generatif'),
-                trailing: Icon(Icons.more_vert),
-              ),
-            ),
-            Card(
-              child: ListTile(
                 leading: Image.asset("assets/img/lior.jpg"),
                 title: Text('Lior Chamla(13h à 13h30)'),
                 subtitle: Text('Le code lagacy'),
-                trailing: Icon(Icons.more_vert),
+                trailing: Icon(Icons.info),
               ),
             ),
           ],
@@ -42,3 +31,4 @@ class EventPage extends StatelessWidget {
     );
   }
 }
+
